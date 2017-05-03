18fa41686f8669d4f8685025055daebedb0aefb6
micka <micka@Mickal>
2017-05-03 17:05:21 +0200

    Ajout de la méthode String dans la classe SpaceInvaders

--- src/main/java/fr/unilim/iut/spaceinvaders/SpaceInvaders.java
+++ src/main/java/fr/unilim/iut/spaceinvaders/SpaceInvaders.java
@@ -6,8 +6,30 @@ public class SpaceInvaders {
     int hauteur;
 
     public SpaceInvaders(int longueur, int hauteur) {
+    	
 	   this.longueur = longueur;
 	   this.hauteur = hauteur;
-   }
+	   
+    }
+    
+    @Override
+    public String toString() {
+    	
+        String s = "";
+        
+        for (int i = 0; i < hauteur; i++) {
+        	
+            for (int j = 0; j < longueur; j++) {
+            	
+                s += ".";
+                
+            }
+            
+            s += "\n";
+            
+        }
+        
+        return s;
+    }
 
 }
