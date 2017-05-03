4bd85e70bd6c2e05474652d3ccdb8af601f7b568
micka <micka@Mickal>
2017-05-03 17:03:02 +0200

    Ajout du test unitaire et mise à jour de la classe SpaceInvaders -> création d'un espace de jeu

--- src/main/java/fr/unilim/iut/spaceinvaders/SpaceInvaders.java
+++ src/main/java/fr/unilim/iut/spaceinvaders/SpaceInvaders.java
@@ -2,4 +2,12 @@ package fr.unilim.iut.spaceinvaders;
 
 public class SpaceInvaders {
 
+    int longueur;
+    int hauteur;
+
+    public SpaceInvaders(int longueur, int hauteur) {
+	   this.longueur = longueur;
+	   this.hauteur = hauteur;
+   }
+
 }
