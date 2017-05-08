510ad0a8fd5e3e35f9b3a3605eeeee510a835ab4
micka <micka@Mickal>
2017-05-08 22:12:07 +0200

    Refactoring du test et finalisation de la fonctionnalité de poistionnement

--- src/test/java/fr/unilim/iut/spaceinvaders/SpaceInvadersTest.java
+++ src/test/java/fr/unilim/iut/spaceinvaders/SpaceInvadersTest.java
@@ -3,16 +3,26 @@ package fr.unilim.iut.spaceinvaders;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import fr.unilim.iut.spaceinvaders.utils.HorsEspaceJeuException;
 
 public class SpaceInvadersTest {
+	
+	private SpaceInvaders spaceinvaders;
+
+    @Before
+    public void initialisation() {
+    	
+	    spaceinvaders = new SpaceInvaders(15, 10);
+	    
+    }
+
 
    @Test
    public void test_AuDebut_JeuSpaceInvaderEstVide() {
 	   
-	    SpaceInvaders spaceinvaders = new SpaceInvaders(15, 10);
 	    assertEquals("" + 
 	    "...............\n" + 
 	    "...............\n" +
@@ -30,7 +40,6 @@ public class SpaceInvadersTest {
    @Test
    public void test_unNouveauVaisseauEstCorrectementPositionneDansEspaceJeu() {
 	   
-		SpaceInvaders spaceinvaders = new SpaceInvaders(15, 10);
 		spaceinvaders.positionnerUnNouveauVaisseau(7,9);
 		assertEquals("" + 
 		"...............\n" + 
@@ -48,8 +57,6 @@ public class SpaceInvadersTest {
    
    @Test
 	public void test_UnNouveauVaisseauPositionneHorsEspaceJeu_DoitLeverUneException() {
-	   
-		SpaceInvaders spaceinvaders = new SpaceInvaders(15, 10);
 		
 		try {
 			
