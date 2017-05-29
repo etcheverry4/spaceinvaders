1583d193f5b3219f59ff678e952a6b247ff7faf8
MathieuE <mathieu.etcheverry@etu.unilim.fr>
2017-05-29 17:02:38 +0200

    # WARNING: head commit changed in the meantime
    Auteur : MathieuE <mathieu.etcheverry@etu.unilim.fr>
    
    Création d'un nouveau commit

---
+++ src/main/java/fr/unilim/iut/spaceinvaders/utils/DebordementEspaceJeuException.java
@@ -0,0 +1,12 @@
+package fr.unilim.iut.spaceinvaders.utils;
+
+@SuppressWarnings("serial")
+public class DebordementEspaceJeuException extends RuntimeException {
+
+    public DebordementEspaceJeuException(String message) {
+    	
+	    super(message);
+	    
+    }
+    
+}
